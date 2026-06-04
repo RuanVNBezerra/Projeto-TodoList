82f743ed38be591daa8ea91a1bdd1f93cb35a410
Lenilson Alves <lenilsonasousa@hotmail.com>
2026-06-04 12:03:39 -0300

    fazendo mudanças em em service apos mudança em repository

--- projeto/src/main/java/com/todo/Service/TarefaService.java
+++ projeto/src/main/java/com/todo/Service/TarefaService.java
@@ -1,47 +1,64 @@
 package com.todo.Service;
 
-import model.Tarefa;
-import model.Usuario;
-import repository.TarefaRepository;
-
 import java.util.List;
+
 
 public class TarefaService {
 
     private TarefaRepository repository;
 
-    public TarefaService(TarefaRepository repository) {
+    public TarefaService(
+            TarefaRepository repository) {
+
         this.repository = repository;
     }
 
-    public void criarTarefa(String titulo, String descricao, Usuario usuario) {
+    public void criarTarefa(
+            String titulo,
+            String descricao,
+            Usuario usuario) {
 
-        if (titulo == null || titulo.isEmpty()) {
+        if (titulo == null ||
+                titulo.isBlank()) {
+
             System.out.println("Título inválido.");
+
             return;
         }
 
         int id = repository.gerarId();
 
-        Tarefa tarefa = new Tarefa(id, titulo, descricao, usuario);
+        Tarefa tarefa =new Tarefa(
+                        id,
+                        titulo,
+                        descricao,
+                        usuario );
 
         repository.salvarTarefa(tarefa);
     }
 
     public List<Tarefa> listarTarefas() {
+
         return repository.listarTarefas();
     }
 
-    public void concluirTarefa(int id) {
+    public void concluirTarefa(
+            int id) {
 
-        for (Tarefa tarefa : repository.listarTarefas()) {
+        for (Tarefa tarefa :
+                repository.listarTarefas()) {
 
             if (tarefa.getId() == id) {
+
                 tarefa.concluir();
+
+                repository.salvarArquivo();
+
                 return;
             }
         }
 
-        System.out.println("Tarefa não encontrada.");
+        System.out.println(
+                "Tarefa não encontrada.");
     }
 }
